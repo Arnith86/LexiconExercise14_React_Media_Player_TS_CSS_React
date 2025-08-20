387b5af12f8b292502e47b2bc61fb0316f96b883
a21jeaha <hisui86@gmail.com>
2025-08-20 08:56:51 +0200

    extracted player into its own component

--- LexiconExercise13_React_Media_Player_TS_CSS_React/src/App.tsx
+++ LexiconExercise13_React_Media_Player_TS_CSS_React/src/App.tsx
@@ -1,29 +1,17 @@
 import { useState } from "react";
 import "./css/base.css";
-import { MediaActionButtons } from "./components/MediaActionButtons";
-import { TrackBar } from "./components/TrackBar";
-import { NowPlaying } from "./components/NowPlaying";
-import { TrackCover } from "./components/TrackCover";
-
 import { tracks } from "./data/tracks";
 import type { IMusicTrack } from "./types/IMusicTrack";
+import { Player } from "./components/Player";
 import { PlayerHeader } from "./components/PlayerHeader";
-import { BackgroundCoverBlurred } from "./components/BackgroundCoverBlurred";
 
 function App() {
-  const testCover: IMusicTrack = tracks[2];
+  const testTrack: IMusicTrack = tracks[2];
   const testPlaylistName: string = "Moody";
   return (
     <>
       <PlayerHeader playListName={testPlaylistName} />
-      <TrackCover
-        imageSrc={testCover.cover}
-        altText={`${testCover.title} cover`}
-      />
-      <NowPlaying artist="Like we used to" trackTitle="A Rocket to the moon" />
-      <TrackBar trackLength={260} />
-      <MediaActionButtons />
-      <BackgroundCoverBlurred coverUrl={testCover.cover} />
+      <Player trackData={testTrack} />
     </>
   );
 }
---
+++ LexiconExercise13_React_Media_Player_TS_CSS_React/src/components/Player.tsx
@@ -0,0 +1,26 @@
+import type { ReactElement } from "react";
+import { TrackCover } from "./TrackCover";
+import { NowPlaying } from "./NowPlaying";
+import { TrackBar } from "./TrackBar";
+import { MediaActionButtons } from "./MediaActionButtons";
+import { BackgroundCoverBlurred } from "./BackgroundCoverBlurred";
+import type { IMusicTrack } from "../types/IMusicTrack";
+
+interface IPlayerProp {
+  trackData: IMusicTrack;
+}
+
+export function Player({ trackData }: IPlayerProp): ReactElement {
+  return (
+    <article>
+      <TrackCover
+        imageSrc={trackData.cover}
+        altText={`${trackData.title} cover`}
+      />
+      <NowPlaying artist="Like we used to" trackTitle="A Rocket to the moon" />
+      <TrackBar trackLength={trackData.length} />
+      <MediaActionButtons />
+      <BackgroundCoverBlurred coverUrl={trackData.cover} />
+    </article>
+  );
+}
